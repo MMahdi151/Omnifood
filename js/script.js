3fa46ec19fdc4463860aea9501affa582aba095e
mmahdi151 <mmahdikarimi2006@gmail.com>
2023-03-24 15:07:01 +0430

    sticky-nav

--- js/script.js
+++ js/script.js
@@ -50,6 +50,21 @@ allLinks.forEach(function (link) {
   });
 });
 
+// Sticky-Nav
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(function (entries) {
+  const ent = entries[0];
+  if (!ent.isIntersecting) {
+    document.querySelector('.header').classList.add('sticky');
+  }
+}, {
+  // In the Viewport
+  root: null,
+  threshold: 0
+});
+obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
@@ -69,8 +84,6 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
 .no-flexbox-gap .main-nav-list li:not(:last-child) {
